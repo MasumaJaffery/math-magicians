da783c378fb47fd6498de7e69dfb5d3d429c4de2
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-13 05:30:05 -0700

    onClick Implement

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,13 @@ import './style.css';
 import AppTitle from './AppTitle';
 
 function Calculator() {
+  function handleNumber(value) {
+    // eslint-disable-next-line no-alert
+    alert(`Clicked ${value}`);
+  }
+  function handleOperator() {
+
+  }
   return (
     <div>
       <AppTitle />
@@ -10,29 +17,29 @@ function Calculator() {
         <div className="display">
           <input type="number" placeholder="0" />
         </div>
-        <CalcBtn value="A/C" />
-        <CalcBtn value="+/-" />
-        <CalcBtn value="%" />
-        <CalcBtn className="operator" value="/" />
+        <CalcBtn value="A/C" onClick={() => handleOperator('A/C')} />
+        <CalcBtn value="+/-" onClick={() => handleOperator('+/-')} />
+        <CalcBtn value="%" onClick={() => handleOperator('%')} />
+        <CalcBtn className="operator" value="/" onClick={() => handleOperator('/')} />
 
-        <CalcBtn value="7" />
-        <CalcBtn value="8" />
-        <CalcBtn value="9" />
-        <CalcBtn className="operator" value="*" />
+        <CalcBtn value="7" onClick={() => handleNumber('7')} />
+        <CalcBtn value="8" onClick={() => handleNumber('8')} />
+        <CalcBtn value="9" onClick={() => handleNumber('9')} />
+        <CalcBtn className="operator" value="*" onClick={() => handleOperator('*')} />
 
-        <CalcBtn value="4" />
-        <CalcBtn value="5" />
-        <CalcBtn value="6" />
-        <CalcBtn className="operator" value="-" />
+        <CalcBtn value="4" onClick={() => handleNumber('4')} />
+        <CalcBtn value="5" onClick={() => handleNumber('5')} />
+        <CalcBtn value="6" onClick={() => handleNumber('6')} />
+        <CalcBtn className="operator" value="-" onClick={() => handleOperator('-')} />
 
-        <CalcBtn value="1" />
-        <CalcBtn value="2" />
-        <CalcBtn value="3" />
-        <CalcBtn className="operator" value="+" />
+        <CalcBtn value="1" onClick={() => handleNumber('1')} />
+        <CalcBtn value="2" onClick={() => handleNumber('2')} />
+        <CalcBtn value="3" onClick={() => handleNumber('3')} />
+        <CalcBtn className="operator" value="+" onClick={() => handleOperator('+')} />
 
-        <CalcBtn className="spanbtn" value="0" />
-        <CalcBtn value="." />
-        <CalcBtn className="operator" value="=" />
+        <CalcBtn className="spanbtn" value="0" onClick={() => handleNumber('0')} />
+        <CalcBtn value="." onClick={() => handleNumber('.')} />
+        <CalcBtn className="operator" value="=" onClick={() => handleOperator('=')} />
       </div>
     </div>
   );
@@ -41,7 +48,7 @@ function CalcBtn(props) {
   return (
   /* eslint-disable react/jsx-one-expression-per-line */
     // eslint-disable-next-line react/destructuring-assignment, react/prop-types
-    <button type="button" className={props.className}> {props.value}</button>
+    <button type="button" className={props.className} onClick={() => props.onClick(props.value)}> {props.value}</button>
   );
 }
 export default Calculator;
