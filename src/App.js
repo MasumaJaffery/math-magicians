5a8d5af0df94202f16e4cc9e60b94d7d197ef6da
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-19 14:26:59 -0700

    Footer Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quotes';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="container">
-      <Calculator />
-      <Quote />
-    </div>
+    <>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+import './style.css';
+
+const Home = () => (
+  <>
+    <p className="content">
+      Welcome to our page!
+      Hello there, math magician! This is the Math Magicians web app. The web app is simple.
+      This Web App facilates you will be able to do some simple calculations
+      using our Web Calculator and refresh your soul with quotes.
+      I hope you like this website. Lets explore the other pages, click on the links above!
+    </p>
+  </>
+);
+
+export default Home;
