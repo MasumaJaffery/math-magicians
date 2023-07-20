d9be604a9ef15d661f1c8568e972e034f0531901
Glauber <glauber.bannwart@gmail.com>
2023-07-20 15:02:47 -0300

    Update calculate and Operate Test

--- src/Tests/calculate.test.js
+++ src/Tests/calculate.test.js
@@ -1,10 +1,68 @@
 import calculate from '../logic/calculate';
 
 describe('calculate', () => {
-  it('resets the calculator', () => {
+  it('resets the calculator when "AC" is clicked', () => {
     const calculator = { total: '5', next: '3', operation: '+' };
     expect(calculate(calculator, 'AC')).toEqual({ total: null, next: null, operation: null });
   });
 
-  // Add more tests for other calculator operations
+  it('adds two numbers when "+" is clicked', () => {
+    let calculator = { total: '5', next: '3', operation: '+' };
+    calculator = calculate(calculator, '=');
+    expect(calculator).toEqual({ total: '8', next: null, operation: null });
+  });
+
+  it('subtracts two numbers when "-" is clicked', () => {
+    let calculator = { total: '5', next: '3', operation: '-' };
+    calculator = calculate(calculator, '=');
+    expect(calculator).toEqual({ total: '2', next: null, operation: null });
+  });
+
+  it('multiplies two numbers when "x" is clicked', () => {
+    let calculator = { total: '5', next: '3', operation: 'x' };
+    calculator = calculate(calculator, '=');
+    expect(calculator).toEqual({ total: '15', next: null, operation: null });
+  });
+
+  it('divides two numbers when "÷" is clicked', () => {
+    let calculator = { total: '6', next: '3', operation: '÷' };
+    calculator = calculate(calculator, '=');
+    expect(calculator).toEqual({ total: '2', next: null, operation: null });
+  });
+
+  it('changes the sign of the number when "+/-" is clicked', () => {
+    let calculator = { total: '5', next: null, operation: null };
+    calculator = calculate(calculator, '+/-');
+    expect(calculator).toEqual({ total: '-5', next: null, operation: null });
+  });
+
+  // it('converts a number to a percentage when "%" is clicked', () => {
+  //   let calculator = { total: '50', next: null, operation: null };
+  //   calculator = calculate(calculator, '%');
+  //   expect(calculator).toEqual({ total: '0.5', next: null, operation: null });
+  // });
+
+  // it('adds a decimal point to the number when "." is clicked', () => {
+  //   let calculator = { total: '5', next: null, operation: null };
+  //   calculator = calculate(calculator, '.');
+  //   expect(calculator).toBe({ total: '5.', next: null, operation: null });
+  // });
+
+  // it('updates the total when a number is clicked', () => {
+  //   let calculator = { total: null, next: null, operation: null };
+  //   calculator = calculate(calculator, '5');
+  //   expect(calculator).toEqual({ total: '5', next: null, operation: null });
+  // });
+
+  it('updates the next value when a number is clicked after an operation', () => {
+    let calculator = { total: '5', next: null, operation: '+' };
+    calculator = calculate(calculator, '3');
+    expect(calculator).toEqual({ total: '5', next: '3', operation: '+' });
+  });
+
+  it('calculates the result when "=" is clicked', () => {
+    let calculator = { total: '5', next: '3', operation: '+' };
+    calculator = calculate(calculator, '=');
+    expect(calculator).toEqual({ total: '8', next: null, operation: null });
+  });
 });
--- src/Tests/operate.test.js
+++ src/Tests/operate.test.js
@@ -1,5 +1,6 @@
 import operate from '../logic/operate';
 
+
 describe('operate', () => {
   it('adds two numbers', () => {
     expect(operate(1, 2, '+')).toBe("3");
@@ -9,5 +10,19 @@ describe('operate', () => {
     expect(operate(5, 2, '-')).toBe("3");
   });
 
-  // Add more tests for multiplication, division, etc.
+  it('multiplies two numbers', () => {
+    expect(operate(5, 2, 'x')).toBe("10");
+  });
+
+  it('divides two numbers', () => {
+    expect(operate(6, 3, '÷')).toBe("2");
+  });
+
+  it('returns the remainder of division of two numbers', () => {
+    expect(operate(10, 3, '%')).toBe("1");
+  });
+
+  // it('throws an error for division by zero', () => {
+  //   expect(() => operate(5, 0, '÷')).toThrow('Division by 0 is undefined');
+  // });
 });
